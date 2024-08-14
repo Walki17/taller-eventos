93534fb1eecb5ce403949636f0703e5c6bcb08a3
Walki17 <walkii_@hotmail.com>
2024-08-14 16:04:09 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 var div = document.getElementById('divi');
+var boton = document.getElementById("saludo");
 
+
+boton.addEventListener('click', function(event){
+    alert("Hola!"); 
+    event.stopPropagation();
+});
 div.addEventListener('click', () => {
     alert('Hola! Soy el div');
 });
